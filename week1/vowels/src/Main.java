f48e369314672ccb0210a2120ba24b9e80c51b61
ferhatsusam <dev.ferhatsusam@gmail.com>
2023-06-03 17:35:26 +0300

    application completed

---
+++ week1/vowels/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        char character = 'I';
+
+
+        switch (character) {
+            case 'A':
+            case 'I':
+            case 'U':
+                System.out.println("Bold vowel");
+                break;
+            default:
+                System.out.println("Thin vowel");
+        }
+
+
+
+    }
+}
