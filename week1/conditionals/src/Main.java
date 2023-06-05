51e28d8d95b898768138ec64bf9a35c7de4c0610
ferhatsusam <dev.ferhatsusam@gmail.com>
2023-06-05 21:48:53 +0300

    application completed

---
+++ week1/conditionals/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+
+    public static void main (String[] args) {
+
+
+        int number1 = 88;
+
+
+        if (number1 < 123)
+            System.out.println("Number is less than 123");
+        else if (number1 == 123)
+            System.out.println("Number equal to 123");
+        else
+            System.out.println("Number greater than 123");
+
+
+    }
+}
