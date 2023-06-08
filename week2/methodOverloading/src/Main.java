da1558eab68b3f7ad8f82954b56a525016684e7a
ferhatsusam <dev.ferhatsusam@gmail.com>
2023-06-08 23:09:52 +0300

    application completed

---
+++ week2/methodOverloading/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        fourOperations fourOperations = new fourOperations();
+
+        System.out.println(fourOperations.total(89, 125));
+        System.out.println(fourOperations.total(18, 73, 56));
+
+    }
+}
---
+++ week2/methodOverloading/src/fourOperations.java
@@ -0,0 +1,14 @@
+public class fourOperations{
+
+        public int total(int number1, int number2) {
+            return number1 + number2;
+
+        }
+
+        public int total(int number1, int number2, int number3) {
+            return number1 + number2 + number3;
+        }
+
+}
+
+
