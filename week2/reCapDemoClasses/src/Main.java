e34686d2008caf44550edc08b1e9f5da596e0df5
ferhatsusam <dev.ferhatsusam@gmail.com>
2023-06-10 13:50:44 +0300

    application completed

---
+++ week2/reCapDemoClasses/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        fourOperations fourOperations = new fourOperations();
+        int total = fourOperations.collection(18, 81);
+        System.out.println(" - Total: " + total);
+    }
+
+}
---
+++ week2/reCapDemoClasses/src/fourOperations.java
@@ -0,0 +1,19 @@
+public class fourOperations {
+
+        public int collection(int number1, int number2) {
+            return number1 + number2;
+        }
+
+        public int removal(int number1, int number2) {
+            return number1 - number2;
+        }
+
+        public int multiplication(int number1, int number2) {
+            return number1 * number2;
+        }
+
+        public int division(int number1, int number2) {
+            return number1 / number2;
+        }
+}
+
