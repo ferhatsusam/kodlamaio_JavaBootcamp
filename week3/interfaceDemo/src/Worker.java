37f24b3c15fc8d88f1be445b6ace4df4f03c30b1
ferhatsusam <dev.ferhatsusam@gmail.com>
2023-06-12 22:28:32 +0300

    demo project finished

---
+++ week3/interfaceDemo/src/Worker.java
@@ -0,0 +1,21 @@
+public class Worker implements IEatable,IPayable, IWorkable{
+
+    @Override
+    public void work() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void eat() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pay() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
